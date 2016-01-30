a8427b6c4df3ba595777a1b5c2199de076f6f2ff
KO Myung-Hun <komh@chollian.net>
2016-01-30 14:58:50 +0900

    package: update packaging scripts
        new file:   .gitattributes
        modified:   kdllar.txt
        deleted:    name.cmd
        modified:   pack.cmd

--- name.cmd
+++
@@ -1,9 +0,0 @@
-extproc sh
-
-# $1 : base name, $2 : version macro, $3 : header
-
-normal_ver=$(sed -n "s/#define $2.*\"\(.*\)\"/\1/p" $3)
-short_ver=$(echo $normal_ver|sed 's/[^0-9]//g')
-
-sed -e "s/@SHORT_VER@/$short_ver/g" -e "s/@VER@/$normal_ver/g" $1.txt > $1$short_ver.txt
-mv $1.zip $1$short_ver.zip
--- pack.cmd
+++ pack.cmd
@@ -1,16 +1,67 @@
-@echo off
-setlocal
+/* pack.cmd to package a project using GNU Make/GCC build system and git */
 
-SET SRC=src.zip
+/****** Configuration parts begin ******/
 
-gmake clean
-gmake RELEASE=1
+sPackageName = 'kdllar'
+sVerMacro = 'KDLLAR_VERSION'
+sVerHeader = 'kdllar.h'
 
-git export > %SRC%
+sDistFiles = 'kdllar.exe',
+             'README'
 
-zip kdllar kdllar.exe README %SRC%
-del %SRC%
+/****** Configuration parts end ******/
 
-call name.cmd kdllar KDLLAR_VERSION kdllar.h
+sCmd = 'sed -n "s/^#define' sVerMacro '*\\\"\(.*\)\\\"$/\1/p"' sVerHeader
+sVer = getOutput( sCmd )
+sShortVer = removeNonNumbers( sVer )
 
-endlocal
+'gmake' 'clean'
+'gmake' 'RELEASE=1'
+
+'sed' '-e' 's/@VER@/' || sVer || '/g',
+      '-e' 's/@SHORT_VER@/' || sShortVer || '/g',
+       sPackageName || '.txt' '>' sPackageName || sShortVer || '.txt'
+
+'git' 'archive' '--format' 'zip' sVer '>' 'src.zip'
+'zip' sPackageName || sShortVer || '.zip' sDistFiles 'src.zip'
+'rm' '-f' 'src.zip'
+
+exit 0
+
+/* Get outputs from commands */
+getOutput: procedure
+    parse arg sCmd
+
+    nl = x2c('d') || x2c('a')
+
+    rqNew = rxqueue('create')
+    rqOld = rxqueue('set', rqNew )
+
+    address cmd sCmd '| rxqueue' rqNew
+
+    sResult = ''
+    do while queued() > 0
+        parse pull sLine
+        sResult = sResult || sLine || nl
+    end
+
+    call rxqueue 'Delete', rqNew
+    call rxqueue 'Set', rqOld
+
+    /* Remove empty lines at end */
+    do while right( sResult, length( nl )) = nl
+        sResult = delstr( sResult, length( sResult ) - length( nl ) + 1 )
+    end
+
+    return sResult
+
+/* Remove non-number characters */
+removeNonNumbers: procedure
+    parse arg sStr
+
+    do i = length( sStr) to 1 by -1
+        if datatype( substr( sStr, i, 1 ), 'n') = 0  then
+            sStr = delstr( sStr, i, 1 )
+    end
+
+    return sStr
